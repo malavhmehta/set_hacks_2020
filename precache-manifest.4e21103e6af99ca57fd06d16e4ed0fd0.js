8cdddf08d85cff45c8f57b0c8dd0c1edfdac2438
malav-mehta <malav.mehta@hotmail.com>
2020-06-27 21:06:59 -0400

    Updates

---
+++ precache-manifest.4e21103e6af99ca57fd06d16e4ed0fd0.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56560b5d883ad17a08b8bb049a8a860e",
+    "url": "/SET-Hacks/index.html"
+  },
+  {
+    "revision": "121f7a112cb9de6f1fcb",
+    "url": "/SET-Hacks/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "c21da3b42f25c08b5f5c",
+    "url": "/SET-Hacks/static/css/main.82bed772.chunk.css"
+  },
+  {
+    "revision": "121f7a112cb9de6f1fcb",
+    "url": "/SET-Hacks/static/js/2.4119211f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/SET-Hacks/static/js/2.4119211f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c21da3b42f25c08b5f5c",
+    "url": "/SET-Hacks/static/js/main.7db28ebe.chunk.js"
+  },
+  {
+    "revision": "efa05479b469123f82d6",
+    "url": "/SET-Hacks/static/js/runtime-main.5135618b.js"
+  },
+  {
+    "revision": "a5de5f5bab8f611e1dc88fa1e69e35b2",
+    "url": "/SET-Hacks/static/media/01.a5de5f5b.png"
+  },
+  {
+    "revision": "06ab3f40db60c9a580b6d32e31eb74e8",
+    "url": "/SET-Hacks/static/media/02.06ab3f40.png"
+  },
+  {
+    "revision": "1cebfd419d31f5cac833d235d526d651",
+    "url": "/SET-Hacks/static/media/03.1cebfd41.png"
+  },
+  {
+    "revision": "d9c14540d19ebea9b79fcc80f6911fc5",
+    "url": "/SET-Hacks/static/media/04.d9c14540.png"
+  },
+  {
+    "revision": "9da33cf448d08c5c835bd5cfb697f8cd",
+    "url": "/SET-Hacks/static/media/05.9da33cf4.png"
+  },
+  {
+    "revision": "553fdba9674d94a00be7ca1c46c02620",
+    "url": "/SET-Hacks/static/media/06.553fdba9.png"
+  },
+  {
+    "revision": "73ac8800291fbfe6d2f34a7a22a3030e",
+    "url": "/SET-Hacks/static/media/07.73ac8800.png"
+  },
+  {
+    "revision": "6d678190abf4b92daa16a7d01ad7884c",
+    "url": "/SET-Hacks/static/media/08.6d678190.png"
+  },
+  {
+    "revision": "b2788404ef0eee1c4b48fa4d96a5ddab",
+    "url": "/SET-Hacks/static/media/09.b2788404.png"
+  },
+  {
+    "revision": "25b8202fc063223edb1fefb02ab4ea65",
+    "url": "/SET-Hacks/static/media/10.25b8202f.png"
+  },
+  {
+    "revision": "0006095026ec44bf65dfa1437f03d8e9",
+    "url": "/SET-Hacks/static/media/11.00060950.png"
+  },
+  {
+    "revision": "01343bf0ef2156949b9b4bada5bf9829",
+    "url": "/SET-Hacks/static/media/12.01343bf0.png"
+  },
+  {
+    "revision": "8b5de4182e777b15e984e8f34f1f161a",
+    "url": "/SET-Hacks/static/media/13.8b5de418.png"
+  },
+  {
+    "revision": "1c637bd183e9b7183062e8eab61a4f0b",
+    "url": "/SET-Hacks/static/media/14.1c637bd1.png"
+  },
+  {
+    "revision": "cac49adc8b513969335ff3863d5d00ba",
+    "url": "/SET-Hacks/static/media/15.cac49adc.png"
+  },
+  {
+    "revision": "49be380b76f519441b1a578424fa76c4",
+    "url": "/SET-Hacks/static/media/16.49be380b.png"
+  },
+  {
+    "revision": "fa21c62fcbe9f904ba172771b401d632",
+    "url": "/SET-Hacks/static/media/17.fa21c62f.png"
+  },
+  {
+    "revision": "6cdbf30b1fe085e88127fb6776dd5c5c",
+    "url": "/SET-Hacks/static/media/1password.6cdbf30b.png"
+  },
+  {
+    "revision": "9291351be459f5e2f8300d7a9b5a7d0e",
+    "url": "/SET-Hacks/static/media/balsamiq.9291351b.png"
+  },
+  {
+    "revision": "5c6aa279c14bd27ac3d3517ac97b6c82",
+    "url": "/SET-Hacks/static/media/bars.5c6aa279.svg"
+  },
+  {
+    "revision": "be6f6adf586acd9973feaeb370751896",
+    "url": "/SET-Hacks/static/media/bugsee.be6f6adf.png"
+  },
+  {
+    "revision": "e6617a4a4d2a6057b357b50250e91375",
+    "url": "/SET-Hacks/static/media/echoar.e6617a4a.png"
+  },
+  {
+    "revision": "a264f71e3cb926a65746b158289102ac",
+    "url": "/SET-Hacks/static/media/linode.a264f71e.png"
+  },
+  {
+    "revision": "149e145995cb7d1b751c2e9c8ef021bd",
+    "url": "/SET-Hacks/static/media/replit.149e1459.png"
+  },
+  {
+    "revision": "c63fc6960574b321c01644a10e9bbf2a",
+    "url": "/SET-Hacks/static/media/tohacks.c63fc696.png"
+  },
+  {
+    "revision": "a47ff262c421cc8667b960b6dfd6043d",
+    "url": "/SET-Hacks/static/media/wolframlanguage.a47ff262.png"
+  }
+]);
